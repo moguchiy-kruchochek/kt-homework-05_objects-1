6cab3d320963080ba48e0b3da37c30612fb0cb9f
moguchiy-kruchochek <starayaglina@gmail.com>
2025-11-22 01:25:39 +0700

    Revert "Attachments!!11"

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,65 +1,5 @@
 package ru.netology
 
-data class Video(
-    val id: Int,
-    val ownerId: Int,
-    val title: String,
-    val duration: Int
-)
-
-data class Audio(
-    val id: Int,
-    val ownerId: Int,
-    val artist: String,
-    val title: String
-)
-
-data class Link(
-    val url: String,
-    val title: String,
-    val description: String
-)
-
-data class Note(
-    val id: Int,
-    val ownerId: Int,
-    val title: String,
-    val text: String
-)
-
-data class Photo(
-    val id: Int,
-    val ownerId: Int,
-    val descriptionText: String,
-    val width: Int,
-    val height: Int
-)
-
-interface Attachment {
-    val type: String
-}
-
-class VideoAttachment(
-    val video: Video,
-    override val type: String = "video") : Attachment
-
-class AudioAttachment(
-    val audio: Audio,
-    override val type: String = "audio") : Attachment
-
-class LinkAttachment(
-    val link: Link,
-    override val type: String = "link") : Attachment
-
-class NoteAttachment(
-    val note: Note,
-    override val type: String = "note") : Attachment
-
-class PhotoAttachment(
-    val photo: Photo,
-    override val type: String = "photo") : Attachment
-
-
 
 data class Likes(
     val count: Int = 10,
@@ -77,8 +17,7 @@ data class Post(
     val isPinned: Boolean?,
     val isFavorite: Boolean?,
     val marksAsAds: Boolean?,
-    val likes: Likes,
-    val attachments: Array<Attachment>
+    val likes: Likes
 )
 
 class WallService {
@@ -108,43 +47,8 @@ class WallService {
 fun main() {
     val service = WallService()
     val likes = Likes()
-
-    val video = Video(1,1,"video_title",50)
-    val videoAttachment = VideoAttachment(video)
-
-    val audio = Audio(2,2,"artist", "song")
-    val audioAttachment = AudioAttachment(audio)
-
-    val photo = Photo(1,1,"description here",640,480)
-    val photoAttachment = PhotoAttachment(photo)
-
-    val note = Note(1, 1,"note","note's text")
-    val noteAttachment = NoteAttachment(note)
-
-    val link = Link("www.url.com","link","good link")
-    val linkAttachment = LinkAttachment(link)
-
-    val post = Post(0,
-        "11.11.11",
-        "text",
-        friendsOnly = null,
-        "author",
-        isPinned = null,
-        isFavorite = null,
-        marksAsAds = null,
-        likes,
-        attachments = arrayOf(videoAttachment, audioAttachment, photoAttachment, noteAttachment, linkAttachment)
-        )
+    val post = Post(0, "11.11.11", "text", friendsOnly = null, "author", isPinned = null, isFavorite = null, marksAsAds = null, likes)
     service.add(post)
-    service.update(Post(1,
-        "03.04.25",
-        "Important text",
-        false,
-        "me",
-        true,
-        false,
-        false,
-        likes,
-        arrayOf(videoAttachment, audioAttachment, photoAttachment, noteAttachment)))
-        println(post)
+    service.update(Post(1, "03.04.25", "Important text", false, "me", true, false, false, likes))
+
 }
--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -1,7 +1,9 @@
 import org.junit.Test
 
 import org.junit.Assert.*
-import ru.netology.*
+import ru.netology.Likes
+import ru.netology.Post
+import ru.netology.WallService
 
 class WallServiceTest {
 
@@ -9,32 +11,7 @@ class WallServiceTest {
     fun add() {
         val service = WallService()
         val likes = Likes()
-
-        val video = Video(1,1,"video_title",50)
-        val videoAttachment = VideoAttachment(video)
-
-        val audio = Audio(2,2,"artist", "song")
-        val audioAttachment = AudioAttachment(audio)
-
-        val photo = Photo(1,1,"description here",640,480)
-        val photoAttachment = PhotoAttachment(photo)
-
-        val note = Note(1, 1,"note","note's text")
-        val noteAttachment = NoteAttachment(note)
-
-        val link = Link("www.url.com","link","good link")
-        val linkAttachment = LinkAttachment(link)
-
-        val post = Post(3,
-            "31.10.21",
-            "text33",
-            true,
-            "NEauthor",
-            true,
-            true,
-            false,
-            likes,
-            arrayOf(videoAttachment,audioAttachment,photoAttachment,noteAttachment,linkAttachment))
+        val post = Post(3, "31.10.21", "text33", true, "NEauthor", true, true, false, likes)
 
         val result = service.add(post)
         assertEquals(1, result.id)
@@ -44,64 +21,11 @@ class WallServiceTest {
     fun updateExistingWithTrueResult() {
         val service = WallService()
         val likes = Likes()
+        service.add(Post(0, "11.11.20", "text51", true, "authorr", true, false, null, likes))
+        service.add(Post(0, "23.12.21", "super text", false, "corp", null, true, false, likes))
+        service.add(Post(0, "03.03.25", "important", null, "mine", true, false, false, likes))
 
-        val video = Video(1,1,"video_title",50)
-        val videoAttachment = VideoAttachment(video)
-
-        val audio = Audio(2,2,"artist", "song")
-        val audioAttachment = AudioAttachment(audio)
-
-        val photo = Photo(1,1,"description here",640,480)
-        val photoAttachment = PhotoAttachment(photo)
-
-        val note = Note(1, 1,"note","note's text")
-        val noteAttachment = NoteAttachment(note)
-
-        val link = Link("www.url.com","link","good link")
-        val linkAttachment = LinkAttachment(link)
-
-        service.add(Post(0,
-            "11.11.20",
-            "text51",
-            true,
-            "authorr",
-            true,
-            false,
-            null,
-            likes,
-            arrayOf(videoAttachment,audioAttachment,photoAttachment,noteAttachment,linkAttachment)))
-        service.add(Post(0,
-            "23.12.21",
-            "super text",
-            false,
-            "corp",
-            null,
-            true,
-            false,
-            likes,
-            arrayOf(videoAttachment,audioAttachment,photoAttachment,noteAttachment,linkAttachment)))
-        service.add(Post(0,
-            "03.03.25",
-            "important",
-            null,
-            "mine",
-            true,
-            false,
-            false,
-            likes,
-            arrayOf(videoAttachment,audioAttachment,photoAttachment,noteAttachment,linkAttachment)))
-
-        val update = Post(3,
-            "03.04.25",
-            "IMPORTANT!",
-            null,
-            "me",
-            null,
-            null,
-            null,
-            likes,
-            arrayOf(videoAttachment,audioAttachment,photoAttachment,noteAttachment,linkAttachment))
-
+        val update = Post(3, "03.04.25", "IMPORTANT!", null, "me", null, null, null, likes)
         val result = service.update(update)
 
         assertTrue("The Post updated!", result)
@@ -111,67 +35,11 @@ class WallServiceTest {
     fun updateWithFalseResult() {
         val service = WallService()
         val likes = Likes()
+        service.add(Post(0, "11.11.20", "text51", true, "authorr", true, false, true, likes))
+        service.add(Post(0, "23.12.21", "super text", false, "corp", false, true, false, likes))
+        service.add(Post(0, "03.03.25", "important", false, "mine", true, false, false, likes))
 
-        val video = Video(1,1,"video_title",50)
-        val videoAttachment = VideoAttachment(video)
-
-        val audio = Audio(2,2,"artist", "song")
-        val audioAttachment = AudioAttachment(audio)
-
-        val photo = Photo(1,1,"description here",640,480)
-        val photoAttachment = PhotoAttachment(photo)
-
-        val note = Note(1, 1,"note","note's text")
-        val noteAttachment = NoteAttachment(note)
-
-        val link = Link("www.url.com","link","good link")
-        val linkAttachment = LinkAttachment(link)
-
-        service.add(Post(0,
-            "11.11.20",
-            "text51",
-            true,
-            "authorr",
-            true,
-            false,
-            true,
-            likes,
-            arrayOf(videoAttachment,audioAttachment,photoAttachment,noteAttachment,linkAttachment))
-            )
-        service.add(Post(0,
-            "23.12.21",
-            "super text",
-            false,
-            "corp",
-            false,
-            true,
-            false,
-            likes,
-            arrayOf(videoAttachment,audioAttachment,photoAttachment,noteAttachment,linkAttachment))
-            )
-        service.add(Post(0,
-            "03.03.25",
-            "important",
-            false,
-            "mine",
-            true,
-            false,
-            false,
-            likes,
-            arrayOf(videoAttachment,audioAttachment,photoAttachment,noteAttachment,linkAttachment))
-            )
-
-        val update = Post(6,
-            "03.04.25",
-            "IMPORTANT!",
-            null,
-            "me",
-            null,
-            null,
-            null,
-            likes,
-            arrayOf(videoAttachment,audioAttachment,photoAttachment,noteAttachment,linkAttachment))
-
+        val update = Post(6, "03.04.25", "IMPORTANT!", null, "me", null, null, null, likes)
         val result = service.update(update)
 
         assertFalse("No Posts with such ID :(", result)
